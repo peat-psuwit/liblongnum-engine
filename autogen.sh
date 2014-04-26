f59cb491b476e8fd397f31d1f387451d580d6019
รัชนันท์ ศรีรัตนเมธ <peathot@hotmail.com>
2014-04-26 10:16:45 +0700

    Edit autogen.sh and add remove-autogen.sh
    Old autogen.sh isn't work on some old system. The fix is to create m4/ directory before execute autoreconf.
    Also, to make sure clean tree, remove-autogen.sh is added to make tree cleaning easier. The script will execute "make distclean" before remove all files generated by autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-autoreconf --install || exit 1
+cd "$(dirname $0)"
+mkdir -p m4/
+autoreconf --install || echo "Please install libtool, autoconf and automake before run this script again."
---
+++ remove-autogen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+[ -f Makefile ] && make distclean
+rm -rf  Makefile.in \
+	aclocal.m4 \
+	autom4te.cache/ \
+	config.guess \
+	config.sub \
+	configure \
+	depcomp \
+	install-sh \
+	ltmain.sh \
+	m4/ \
+	missing
